b5a015d4211aa38ad7bfa9822f8dd290caf505a3
Astrid Yu <astrid@astrid.tech>
2023-09-21 01:11:01 -0700

    implement vfs

--- src/udf/dirent.rs
+++ src/udf/dirent.rs
@@ -3,17 +3,23 @@ use std::{
 };
 
 use libcdio_sys::{
-    udf_dirent_free, udf_dirent_t, udf_get_file_length, udf_get_filename, udf_get_link_count,
-    udf_get_posix_filemode, udf_is_dir, udf_opendir, udf_readdir, udf_t,
+    udf_dirent_free, udf_dirent_t, udf_enum1_t_UDF_BLOCKSIZE, udf_fopen, udf_get_file_length,
+    udf_get_filename, udf_get_link_count, udf_get_posix_filemode, udf_is_dir, udf_opendir,
+    udf_read_block, udf_readdir, udf_t,
 };
+
+pub const UDF_BLOCKSIZE: usize = udf_enum1_t_UDF_BLOCKSIZE as usize;
 
 /// Represents an owned, non-null `udf_dirent_s*`.
 pub struct Dirent<'udf> {
     /// This is an [Option] because we might sometimes move it out of
     /// this struct, so calling `drop()` is unnecessary.
-    ptr: *const udf_dirent_t,
+    pub ptr: *const udf_dirent_t,
     _udf: PhantomData<&'udf udf_t>,
 }
+
+unsafe impl Send for Dirent<'_> {}
+unsafe impl Sync for Dirent<'_> {}
 
 impl<'udf> Dirent<'udf> {
     pub fn new(ptr: *const udf_dirent_t) -> Option<Self> {
@@ -82,6 +88,38 @@ impl<'udf> Dirent<'udf> {
     pub fn posix_filemode(&self) -> Permissions {
         let mode = unsafe { udf_get_posix_filemode(self.ptr) };
         Permissions::from_mode(mode)
+    }
+
+    pub fn fopen(&self, path: &str) -> Option<Dirent<'udf>> {
+        let dirent =
+            unsafe { udf_fopen(self.ptr as *mut _, path.as_bytes().as_ptr() as *const i8) };
+        Dirent::new(dirent)
+    }
+
+    pub fn read_block(&self, buf: &mut [u8], block_count: usize) -> Result<usize, std::io::Error> {
+        assert_eq!(buf.len() * UDF_BLOCKSIZE, block_count);
+
+        let initial_buf_len = buf.len();
+
+        unsafe {
+            // Read into the end of the buffer and record result
+            let write_start = buf.as_mut_ptr();
+            let result = udf_read_block(self.ptr, write_start as *mut _, block_count);
+
+            match usize::try_from(result) {
+                Ok(len) => Ok(len),
+                Err(_) => {
+                    // Unsuccessful read.
+                    // SAFETY: We don't need to do anything to the vec because we
+                    // wrote outside of the length.
+                    todo!()
+                }
+            }
+        }
+    }
+
+    pub fn idx(&self) -> u32 {
+        unsafe { self.ptr.as_ref().unwrap().i_loc }
     }
 }
 
--- src/udf/fs.rs
+++ src/udf/fs.rs
@@ -1,18 +1,29 @@
-use std::{os::unix::prelude::OsStrExt, path::Path, ptr};
+use std::{
+    fmt::Debug,
+    io::Read,
+    os::unix::prelude::OsStrExt,
+    path::Path,
+    ptr,
+    sync::{Arc, RwLock},
+};
 
-use libcdio_sys::{udf_close, udf_get_root, udf_open, udf_s};
+use libcdio_sys::{udf_close, udf_fopen, udf_get_root, udf_open, udf_s};
+use vfs::{error::VfsErrorKind, FileSystem, VfsError, VfsResult};
 
-use super::dirent::Dirent;
+use super::dirent::{Dirent, UDF_BLOCKSIZE};
 
 /// An opened UDF filesystem.
 ///
 /// The underlying file pointer is freed on drop.
-pub struct UdfFs {
+struct Inner {
     p_udf: *mut udf_s,
 }
 
-impl UdfFs {
-    pub fn open(path: impl AsRef<Path>) -> UdfFs {
+unsafe impl Send for Inner {}
+unsafe impl Sync for Inner {}
+
+impl Inner {
+    pub fn open(path: impl AsRef<Path>) -> Inner {
         let path = path.as_ref();
         let p_udf = unsafe { udf_open(path.as_os_str().as_bytes().as_ptr() as *const i8) };
         if p_udf == ptr::null_mut() {
@@ -22,17 +33,112 @@ impl UdfFs {
         Self { p_udf }
     }
 
-    fn root<'a>(&'a self) -> Option<Dirent<'a>> {
+    fn root<'udf>(&'udf self) -> Option<Dirent<'udf>> {
         let dirent = unsafe { udf_get_root(self.p_udf, 1, 0) };
         Dirent::new(dirent)
     }
+
+    fn fopen<'udf>(&'udf self, path: &str) -> Option<Dirent<'udf>> {
+        self.root()?.fopen(path)
+    }
 }
 
-impl Drop for UdfFs {
+impl Drop for Inner {
     fn drop(&mut self) {
         unsafe {
             udf_close(self.p_udf);
         }
+    }
+}
+
+pub struct UdfFs {
+    inner: Inner,
+}
+
+impl UdfFs {
+    pub fn open(path: impl AsRef<Path>) -> UdfFs {
+        let fs = Inner::open(path);
+        Self { inner: fs.into() }
+    }
+}
+
+impl FileSystem for UdfFs {
+    fn read_dir(&self, path: &str) -> vfs::VfsResult<Box<dyn Iterator<Item = String> + Send>> {
+        if let Some(mut dirent) = self.inner.fopen(path) {
+            let mut children = vec![];
+            loop {
+                children.push(dirent.file_name().unwrap().to_string_lossy().to_string());
+                match dirent.next_sibling() {
+                    Some(s) => dirent = s,
+                    None => break Ok(Box::new(children.into_iter())),
+                }
+            }
+        } else {
+            Err(VfsError::from(VfsErrorKind::FileNotFound))
+        }
+    }
+
+    fn open_file(&self, path: &str) -> vfs::VfsResult<Box<dyn vfs::SeekAndRead + Send>> {
+        todo!()
+    }
+
+    fn metadata(&self, path: &str) -> vfs::VfsResult<vfs::VfsMetadata> {
+        todo!()
+    }
+
+    fn exists(&self, path: &str) -> vfs::VfsResult<bool> {
+        Ok(self.inner.fopen(path).is_some())
+    }
+
+    fn create_dir(&self, _path: &str) -> vfs::VfsResult<()> {
+        Err(VfsError::from(VfsErrorKind::NotSupported))
+    }
+
+    fn create_file(&self, _path: &str) -> vfs::VfsResult<Box<dyn std::io::Write + Send>> {
+        Err(VfsError::from(VfsErrorKind::NotSupported))
+    }
+
+    fn append_file(&self, _path: &str) -> vfs::VfsResult<Box<dyn std::io::Write + Send>> {
+        Err(VfsError::from(VfsErrorKind::NotSupported))
+    }
+
+    fn remove_file(&self, _path: &str) -> vfs::VfsResult<()> {
+        Err(VfsError::from(VfsErrorKind::NotSupported))
+    }
+
+    fn remove_dir(&self, _path: &str) -> vfs::VfsResult<()> {
+        Err(VfsError::from(VfsErrorKind::NotSupported))
+    }
+}
+
+impl Debug for UdfFs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("UdfFs").finish()
+    }
+}
+
+pub struct UdfFile<'udf> {
+    dirent: Dirent<'udf>,
+    idx: usize,
+    buf: Vec<u8>,
+}
+
+impl<'udf> UdfFile<'udf> {
+    pub fn new(dirent: Dirent<'udf>) -> Self {
+        Self {
+            dirent,
+            idx: 0,
+            buf: vec![],
+        }
+    }
+}
+
+impl<'udf> Read for UdfFile<'udf> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let end = self.idx + buf.len();
+        let start_block = self.idx / UDF_BLOCKSIZE;
+        let end_block = end / UDF_BLOCKSIZE;
+        self.dirent.read_block(buf, block_count)
     }
 }
 
